0791d3ad3169ee115dc5cf9c2faddc8700b4d10f
Junior Martins <amjr.box@gmail.com>
2023-04-17 08:04:22 -0300

    test(products): implementa o início do teste de products

---
+++ src/entities/products.spec.ts
@@ -0,0 +1,11 @@
+import { describe, expect, test } from '@jest/globals'
+
+describe('Products', () => {
+  it('should return an array of products', () => {
+    const products = [
+      { id: 1, name: 'Product 1', price: 10 },
+      { id: 2, name: 'Product 2', price: 20 },
+      { id: 3, name: 'Product 3', price: 30 },
+    ]
+  })
+})
